dfff6b0eba612f280236badb0204c3eaa9b32bc0
Brian Haley <brian_haley@live.ca>
2020-04-22 23:14:38 -0400

    Requested variants

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+  const data = await response.json()
+  const variants = data.variants
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })

7ea5c3f8ff8494e4a4356d9eb74e51c2621ca798
Brian Haley <brian_haley@live.ca>
2020-04-22 23:34:03 -0400

    Prettier formatting and working variant response

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
-  const data = await response.json()
-  const variants = data.variants
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const response = await fetch(
+    "https://cfw-takehome.developers.workers.dev/api/variants"
+  );
+  const data = await response.json();
+  const variants = data.variants;
+  const variant_1 = await fetch(variants[0]);
+  const response_1 = await variant_1.text();
+  console.log(response_1);
+  return new Response(response_1, {
+    headers: { "content-type": "text/html" },
+  });
 }

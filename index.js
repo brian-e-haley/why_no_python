edf7288199bcee1760300694a1d28825177a954e
Brian Haley <brian_haley@live.ca>
2020-04-24 15:31:24 -0400

    Working cookies

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const COOKIE_NAME = "variant";
+
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
@@ -7,8 +9,8 @@ function getRandomInt(max) {
 }
 
 function choose_variant(variants) {
-  const number = getRandomInt(variants.length) + 1
-  return {html: variants[number - 1], number: number};
+  const number = getRandomInt(variants.length) + 1;
+  return { html: variants[number - 1], number: number };
 }
 
 class ElementHandler {
@@ -42,6 +44,23 @@ class ElementHandler {
  * Respond with hello worker text
  * @param {Request} request
  */
+
+function getCookie(request, name) {
+  let result = null;
+  let cookieString = request.headers.get("Cookie");
+  if (cookieString) {
+    let cookies = cookieString.split(";");
+    cookies.forEach((cookie) => {
+      let cookieName = cookie.split("=")[0].trim();
+      if (cookieName === name) {
+        let cookieVal = cookie.split("=")[1];
+        result = cookieVal;
+      }
+    });
+  }
+  return result;
+}
+
 async function handleRequest(request) {
   const response = await fetch(
     "https://cfw-takehome.developers.workers.dev/api/variants"
@@ -49,7 +68,11 @@ async function handleRequest(request) {
   const data = await response.json();
   const variants = data.variants;
 
-  const variant = choose_variant(variants);
+  const cookie = getCookie(request, COOKIE_NAME);
+  const variant = cookie
+    ? { html: variants[cookie - 1], number: cookie }
+    : choose_variant(variants);
+
   const variant_data = await fetch(variant.html);
   const variant_html = await variant_data.text();
 
@@ -57,20 +80,27 @@ async function handleRequest(request) {
     headers: { "content-type": "text/html" },
   });
 
+  if (!cookie) {
+    original_response.headers.append(
+      `Set-Cookie`,
+      `${COOKIE_NAME}=${variant.number}`
+    );
+  }
+
   const rewriter = new HTMLRewriter()
-  .on("title", new ElementHandler({ content: "This is new!" }))
-  .on("h1#title", new ElementHandler({ content: "This is also new!" }))
-  .on(
-    "p#description",
-    new ElementHandler({ content: `This is variant ${variant.number}!` })
-  )
-  .on(
-    "a#url",
-    new ElementHandler({
-      content: "Take me to the repo!",
-      url: "https://github.com/brian-e-haley/why_no_python",
-    })
-  );
+    .on("title", new ElementHandler({ content: "This is new!" }))
+    .on("h1#title", new ElementHandler({ content: "This is also new!" }))
+    .on(
+      "p#description",
+      new ElementHandler({ content: `This is variant ${variant.number}!` })
+    )
+    .on(
+      "a#url",
+      new ElementHandler({
+        content: "Take me to the repo!",
+        url: "https://github.com/brian-e-haley/why_no_python",
+      })
+    );
 
   const new_response = rewriter.transform(original_response);
   return new_response;
